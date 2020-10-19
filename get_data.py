9d5bb7cdf55f7a8f44018766aed542546e48412e
109971008 <109971008@nccu.edu.tw>
2020-10-19 14:11:25 +0000

    Add get data

--- get_data.py
+++ get_data.py
@@ -1,35 +1,19 @@
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from math import pow
-from random import random
 import pandas
 import pandas_datareader as pdr
 from datetime import datetime
-import operator
 
-import findspark
-#use python -m pip install xxxx for missing package
+sd = datetime(2020, 1, 1)
+ed = datetime(2020, 9, 4)
 
-os.environ["SPARK_HOME"] = '/opt/apache-spark/spark-3.0.1-bin-hadoop2.7'
-findspark.init()
+vix = pdr.get_data_yahoo(symbols='^vix', start=sd, end=ed)["Adj Close"]
+spx = pdr.get_data_yahoo(symbols='^SP500TR', start=sd, end=ed)["Adj Close"]
+vfx = pdr.get_data_yahoo(symbols='VFINX', start=sd, end=ed)["Adj Close"]
+vbx = pdr.get_data_yahoo(symbols='VBMFX', start=sd, end=ed)["Adj Close"]
+rom = pdr.get_data_yahoo(symbols='ROMO', start=sd, end=ed)["Adj Close"]
+vmt = pdr.get_data_yahoo(symbols='VMOT', start=sd, end=ed)["Adj Close"]
 
-import pyspark
-from pyspark.ml.regression import LinearRegression
-from pyspark.sql import SparkSession
-from pyspark.ml.linalg import Vectors
-from pyspark.ml.feature import VectorAssembler
+bnp = pandas.concat([vix ,vfx ,vbx ,rom, vmt, spx], axis=1)
+bnp = bnp.sort_values(by="Date",ascending=False)
+bnp.head(80)
 
-start_date = datetime(1990, 1, 1)
-end_date = datetime(2020, 8, 31)
-
-#Update Here! get more feature if needed
-spx = pdr.get_data_yahoo(symbols='^SP500TR', start=start_date, end=end_date)["Adj Close"]
-vix = pdr.get_data_yahoo(symbols='^vix', start=start_date, end=end_date)["Adj Close"]
-bnp = pandas.concat([spx, vix], axis=1)
-#Update Here! update column name base on features
-bnp.columns = ('spx','vix')
-bnp = bnp.sort_values(by="Date",ascending=True)
-print(bnp.head(10))
 bnp.to_csv('Result.csv')
