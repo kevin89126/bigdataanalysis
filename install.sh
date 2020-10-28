703bed1d4bd5c62695d4d10fe387270d5ffb7a45
109971008 <109971008@nccu.edu.tw>
2020-10-28 15:32:04 +0000

    Install tensorflow keara

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -e
 wget https://downloads.apache.org/spark/spark-3.0.1/spark-3.0.1-bin-hadoop2.7.tgz
 apt install default-jre
 tar -zxvf spark-3.0.1-bin-hadoop2.7.tgz
 mkdir -p /opt/apache-spark/
-mv spark-3.0.1-bin-hadoop2.7.tgz /opt/apache-spark/
+mkdir -p ./data
+mv ./spark-3.0.1-bin-hadoop2.7 /opt/apache-spark/
 pip install -r requirements.txt
 rm -rf spark-3.0.1-bin-hadoop2.7.tgz
